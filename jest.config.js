214caffc56b38faf27356a88b427f1fe58365368
Rajeev Karamcheti <rkaramc@gmail.com>
2022-09-14 19:18:03 +0530

    config jest

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,5 @@ module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/tests/prisma-mock.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/prisma-test-data.ts', '<rootDir>/tests/prisma-mock.ts'],
 };
-process.env = Object.assign(process.env, {
-  DATABASE_URL: 'postgresql://realworld@localhost:5432/realworld?schema=public',
-});
